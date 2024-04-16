19f302bd294340e939d34284f0eec3d16522f717
razouck <14312156+razouck@users.noreply.github.com>
2024-04-16 19:40:36 -0300

    refactor code in indent-guides.lua

--- .config/nvim/lua/plugins/indent-guides.lua
+++ .config/nvim/lua/plugins/indent-guides.lua
@@ -1,28 +1,34 @@
 return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
-		opts = {
-			indent = {
-				char = "│",
-				tab_char = "│",
-			},
-			scope = { enabled = false },
-			exclude = {
-				filetypes = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
-				},
-			},
-		},
 		main = "ibl",
+		config = function ()
+			-- I'd do something here if the documentation was useful.
+
+			require("ibl").setup({
+				indent = {
+					char = "│",
+					tab_char = "│",
+				},
+				scope = {
+					enabled = false,
+				},
+				exclude = {
+					filetypes = {
+						"help",
+						"alpha",
+						"dashboard",
+						"neo-tree",
+						"Trouble",
+						"trouble",
+						"lazy",
+						"mason",
+						"notify",
+						"toggleterm",
+						"lazyterm",
+					},
+				},
+			})
+		end,
 	}
 }
