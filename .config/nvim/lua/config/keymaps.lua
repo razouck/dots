8f5e91232b991a3df4cf3bb4cd4d56169fd85671
razouck <14312156+razouck@users.noreply.github.com>
2024-04-18 00:07:20 -0300

    reorganize keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,48 +7,32 @@ local function m(mode, remap, action, description)
 		set(mode, remap, action, { desc = description })
 	end
 end
+local substitution_command = [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]
 
----------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------
 
--- Clear search with <esc>
-m({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", "Escape and Clear hlsearch")
+m({ "i", "n" }     , "<esc>"     , "<cmd>noh<cr><esc>" , "Escape and clear hlsearch"                  )
 
--- Clipboard
-m({ "n", "v" }, "<leader>y", '"+y', "Copy to clipboard")
-m("n", "<leader>Y", '"+yg_', "Copy content after the cursor to clipboard")
-m("n", "<leader>yy", '"+yy', "Copy line to clipboard")
-m("n", "<leader>p", '"+p', "Paste from clipboard")
-m("n", "<leader>P", '"+P', "Paste from clipboard (before cursor)")
+m({ "n", "v" }     , "<leader>y" , '"+y'               , "Copy to clipboard"                          )
+m("n"              , "<leader>Y" , '"+yg_'             , "Copy content after the cursor to clipboard" )
+m("n"              , "<leader>yy", '"+yy'              , "Copy line to clipboard"                     )
+m("n"              , "<leader>p" , '"+p'               , "Paste from clipboard"                       )
+m("n"              , "<leader>P" , '"+P'               , "Paste from clipboard (before cursor)"       )
 
--- File Navigation
-m("n", "<leader>fx", vim.cmd.Ex, "Open file manager")
+m("v"              , "<"         , "<gv"               , "Indent selection"                           )
+m("v"              , ">"         , ">gv"               , "Unindent selection"                         )
 
--- Highlights under remaprsor
-m("n", "<leader>ui", vim.show_pos, "Inspect Pos")
+m("v"              , "J"         , ":m '>+1<cr>gv=gv"  , "Move selection up"                          )
+m("v"              , "K"         , ":m '<-2<cr>gv=gv"  , "Move selection down"                        )
 
--- Indenting
-m("v", "<", "<gv", "Indent selection")
-m("v", ">", ">gv", "Unindent selection")
+m("n"              , "<leader>fn", "<cmd>enew<cr>"     , "New File"                                   )
+m("n"              , "<leader>fx", vim.cmd.Ex          , "Open file manager"                          )
 
--- Lazy
-m("n", "<leader>l", "<cmd>Lazy<cr>", "Lazy")
+m("n"              , "<leader>l" , "<cmd>Lazy<cr>"     , "Lazy"                                       )
+m("n"              , "<leader>m" , "<cmd>Mason<cr>"    , "Mason"                                      )
 
--- Mason
-m("n", "<leader>m", "<cmd>Mason<cr>", "Mason")
+m("n"              , "<leader>s" , substitution_command, "Replace string under cursor"                )
 
--- Move lines
-m("v", "J", ":m '>+1<cr>gv=gv", "Move selection up")
-m("v", "K", ":m '<-2<cr>gv=gv", "Move selection down")
-
--- New File
-m("n", "<leader>fn", "<cmd>enew<cr>", "New File")
-
--- Search
-m("n", "n", "nzzzv")
-m("n", "N", "Nzzzv")
-
--- Substitution
-m("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
--- Unmap
-m("n", "Q", "<nop>")
+m("n"              , "n"         , "nzzzv"                                                            )
+m("n"              , "N"         , "Nzzzv"                                                            )
+m("n"              , "Q"         , "<nop>"                                                            )
