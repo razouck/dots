69508865cbc8aeeea0d241567d7032560f397582
razouck <14312156+razouck@users.noreply.github.com>
2024-04-18 15:15:34 -0300

    refactor comment.lua

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,6 +1,3 @@
-local plugin = {}
-
---------------------------------------------------------------------------------
 
 local keys = {
 	{ "gc"  , desc = "Comment"      },
@@ -9,40 +6,47 @@ local keys = {
 
 --------------------------------------------------------------------------------
 
-local comment = { "echasnovski/mini.comment" }
+local M = {}
 
-plugin[1] = comment
+--------------------------------------------------------------------------------
 
-comment.event = "VeryLazy"
-comment.keys  = keys
-comment.opts  = {}
+local comment                     = { "echasnovski/mini.comment" }
 
-comment.opts.mappings = {}
-comment.opts.options  = {}
+comment.event                     = "VeryLazy"
+comment.keys                      = keys
+comment.opts                      = {}
 
-comment.opts.mappings.comment        = keys[1][1]
-comment.opts.mappings.comment_line   = keys[2][1]
-comment.opts.mappings.comment_visual = keys[1][1]
-comment.opts.mappings.text_object    = keys[1][1]
+local opts                        = comment.opts
 
-comment.opts.options.custom_commentstring = function ()
+opts.mappings                     = {}
+opts.options                      = {}
+
+opts.mappings.comment             = keys[1][1]
+opts.mappings.comment_line        = keys[2][1]
+opts.mappings.comment_visual      = keys[1][1]
+opts.mappings.text_object         = keys[1][1]
+
+opts.options.custom_commentstring = function ()
+
 	local internal  = require("ts_context_commentstring.internal")
 	local calculate = internal.calculate_commentstring
 
 	return calculate() or vim.bo.commentstring
 end
 
---------------------------------------------------------------------------------
-
-local ts_comment = { "JoosepAlviste/nvim-ts-context-commentstring" }
-
-plugin[2] = ts_comment
-
-ts_comment.lazy = true
-ts_comment.opts = {}
-
-ts_comment.opts.enable_autocmd = false
+table.insert(M, comment)
 
 --------------------------------------------------------------------------------
 
-return plugin
+local ts               = { "JoosepAlviste/nvim-ts-context-commentstring" }
+
+ts.lazy                = true
+ts.opts                = {}
+
+ts.opts.enable_autocmd = false
+
+table.insert(M, ts)
+
+--------------------------------------------------------------------------------
+
+return M
