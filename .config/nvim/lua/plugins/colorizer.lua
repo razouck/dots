680b0ae1e8497a413060df7117a0a5fd46cecc35
razouck <14312156+razouck@users.noreply.github.com>
2024-04-18 15:15:04 -0300

    refactor colorizer.lua

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,6 +1,8 @@
-return {
-	"brenoprata10/nvim-highlight-colors",
-	config = function()
-		require("nvim-highlight-colors").turnOn()
-	end,
-}
+
+local plugin = { "brenoprata10/nvim-highlight-colors" }
+
+plugin.config = function()
+	require("nvim-highlight-colors").turnOn()
+end
+
+return plugin
