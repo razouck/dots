34162ee453e2c59a38607983d6771dfc23a8723c
razouck <14312156+razouck@users.noreply.github.com>
2024-04-18 15:31:34 -0300

    refactor indent-guides.lua

--- .config/nvim/lua/plugins/indent-guides.lua
+++ .config/nvim/lua/plugins/indent-guides.lua
@@ -1,32 +1,36 @@
-local function config()
-	require("ibl").setup({
-		indent = {
-			char = "│",
-			tab_char = "│",
-		},
-		scope = { enabled = false, },
-		exclude = {
-			filetypes = {
-				"alpha",
-				"dashboard",
-				"help",
-				"lazy",
-				"lazyterm",
-				"mason",
-				"neo-tree",
-				"notify",
-				"toggleterm",
-				"trouble",
-				"Trouble",
-			},
-		},
-	})
-end
 
-return {
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		config = config,
-	}
+local settings             = {}
+
+settings.indent            = {}
+settings.scope             = {}
+settings.exclude           = {}
+
+settings.indent.char       = "│"
+settings.indent.tab_char   = "│"
+
+settings.scope.enabled     = false
+
+settings.exclude.filetypes = {
+	"alpha",
+	"dashboard",
+	"help",
+	"lazy",
+	"lazyterm",
+	"mason",
+	"neo-tree",
+	"notify",
+	"toggleterm",
+	"trouble",
+	"Trouble",
 }
+
+--------------------------------------------------------------------------------
+
+local M  = { "lukas-reineke/indent-blankline.nvim" }
+
+M.main   = "ibl"
+M.config = function() require("ibl").setup(settings) end
+
+--------------------------------------------------------------------------------
+
+return M
