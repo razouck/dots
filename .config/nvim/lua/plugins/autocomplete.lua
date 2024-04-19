da68d4d5d8f04c40e349842c79ac412352fe0529
razouck <14312156+razouck@users.noreply.github.com>
2024-04-19 18:29:15 -0300

    add autocomplete.lua

---
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -0,0 +1,170 @@
+-- Change this if you want to make a mess.
+local auto_complete_keys = {
+	{ "<C-CR>"   , desc = "Abort Completion"             },
+	{ "<CR>"     , desc = "Choose selection"             },
+	{ "<S-CR>"   , desc = "Choose selection and replace" },
+	{ "<C-b>"    , desc = "Scroll docs up"               },
+	{ "<C-f>"    , desc = "Scroll docs down"             },
+	{ "<S-Tab>"  , desc = "Select previous item"         },
+	{ "<Tab>"    , desc = "Select next item"             },
+	{ "<C-Space>", desc = ""                             }
+}
+
+local function auto_complete_settings()
+	vim.api.nvim_set_hl(
+	0,
+	"CmpGhostText",
+	{ link = "Comment", default = true }
+	)
+
+	local cmp = require("cmp")
+	local defaults = require("cmp.config.default")()
+	local has_words_before = function()
+		unpack = unpack or table.unpack
+		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	end
+	local luasnip = require("luasnip")
+	local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+
+	cmp.event:on(
+	'confirm_done',
+	cmp_autopairs.on_confirm_done()
+	)
+
+	return {
+		auto_brackets = {},
+		completion = {
+			completeopt = "menu,menuone,noinsert",
+		},
+
+		mapping = cmp.mapping.preset.insert({
+			["<C-b>"]     = cmp.mapping.scroll_docs(-4),
+			["<C-f>"]     = cmp.mapping.scroll_docs(4),
+			["<C-Space>"] = cmp.mapping.complete(),
+			["<CR>"]      = cmp.mapping.confirm({ select = true }),
+			["<S-CR>"]    = cmp.mapping.confirm({
+				behavior = cmp.ConfirmBehavior.Replace,
+				select = true,
+			}),
+			["<C-CR>"]    = function(fallback)
+				cmp.abort()
+				fallback()
+			end,
+			["<Tab>"]     = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif luasnip.expand_or_jumpable() then
+					luasnip.expand_or_jump()
+				elseif has_words_before() then
+					cmp.complete()
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+
+			["<S-Tab>"]   = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif luasnip.jumpable(-1) then
+					luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+		}),
+
+		-- IMPORTANT
+		sources = cmp.config.sources(
+		{
+			{ name = "nvim_lsp" },
+			{ name = "path" },
+		},
+		{
+			{ name = "buffer" },
+		}),
+
+		sorting = defaults.sorting,
+	}
+end
+
+
+return {
+	-- Auto complete
+	{
+		"hrsh7th/nvim-cmp",
+		version = false, -- last release is way too old
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+		},
+		opts = auto_complete_settings,
+		keys = auto_complete_keys,
+
+		---@param opts cmp.ConfigSchema | {auto_brackets?: string[]}
+		config = function(_, opts)
+			for _, source in ipairs(opts.sources) do
+				source.group_index = source.group_index or 1
+			end
+
+			local cmp = require("cmp")
+			local Kind = cmp.lsp.CompletionItemKind
+
+			cmp.setup(opts)
+			cmp.event:on("confirm_done", function(event)
+				if not vim.tbl_contains(opts.auto_brackets or {}, vim.bo.filetype) then
+					return
+				end
+
+				local entry = event.entry
+				local item = entry:get_completion_item()
+
+				if vim.tbl_contains({ Kind.Function, Kind.Method }, item.kind) then
+					local keys = vim.api.nvim_replace_termcodes("()<left>", false, false, true)
+
+					vim.api.nvim_feedkeys(keys, "i", true)
+				end
+			end)
+		end,
+	},
+
+	-- Auto pairs
+	{
+		'windwp/nvim-autopairs',
+		event = "InsertEnter",
+		config = true,
+	},
+
+	-- Snippets
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			{
+				"rafamadriz/friendly-snippets",
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+				end,
+			},
+			{
+				"nvim-cmp",
+				dependencies = {
+					"saadparwaiz1/cmp_luasnip",
+				},
+				opts = function(_, opts)
+					opts.snippet = {
+						expand = function(args)
+							require("luasnip").lsp_expand(args.body)
+						end,
+					}
+					table.insert(opts.sources, { name = "luasnip" })
+				end,
+			},
+		},
+		opts = {
+			history = true,
+			delete_check_events = "TextChanged",
+		},
+	},
+}
