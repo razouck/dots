7689e7e58acc0fffc225b30182be121f4fb2f4bd
razouck <14312156+razouck@users.noreply.github.com>
2024-04-23 12:49:50 -0300

    disable intro, change transparency, change comments

--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -1,10 +1,11 @@
 
+local c                      = vim.cmd
 local g                      = vim.g
 local o                      = vim.o
 
 -- Colors
 o.background                 = "light" -- Set light background.
-o.pumblend                   = 30      -- Enable transparency in popup menus.
+o.pumblend                   = 21      -- Enable transparency in popup menus.
 o.termguicolors              = true    -- Enable true terminal colors.
 
 -- Cursor
@@ -13,19 +14,21 @@ o.cursorline                 = true    -- Highlight cursor line.
 o.guicursor                  = ""      -- Always use the block cursor.
 
 -- Indentation
-g.markdown_recommended_style = 0       -- Allow tabs to be used in Markdown.
-g.python_recommended_style   = 0       -- Allow tabs to be used in Python.
+g.markdown_recommended_style = 0       -- Disallow overriding from Markdown.
+g.python_recommended_style   = 0       -- Disallow overriding from Python.
 o.expandtab                  = false   -- Use tabs only.
 o.shiftwidth                 = 4       -- Set tab size.
-o.smartindent                = true    -- Enable smart indentation.
-o.softtabstop                = 0       -- Don't move with spaces with <Tab>.
-o.tabstop                    = 4       -- Amount of spaces inserted by tabs.
+o.smartindent                = true    -- New lines automatically indented.
+o.softtabstop                = 0       -- Don't move by spaces with <Tab>.
+o.tabstop                    = 4       -- <Tab> inserts 4 spaces.
 
 -- Line wrapping
 o.wrap                       = false   -- Disable line wrapping.
 
 -- Mouse
 o.mouse                      = "a"     -- Always allow mouse usage.
+c([[aunmenu PopUp.How-to\ disable\ mouse]])
+c([[aunmenu PopUp.-1-]])               -- Disable useless pop up menu option.
 
 -- Numbers
 o.number                     = true    -- Show the current line number.
@@ -44,7 +47,7 @@ o.splitbelow                 = true    -- Put new view below the current one.
 o.splitright                 = true    -- Put new view to the right.
 
 -- Start Message
-o.shortmess                  = "filnxtToOI" -- Disable intro.
+o.shortmess                  = o.shm.."I" -- Disable Intro message.
 
 -- Undo
 o.undofile                   = true    -- Save undo history for later usage.
