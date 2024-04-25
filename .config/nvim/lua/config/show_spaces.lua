4efe1d903a228dd86e47c3f05b394c963a7c6ccb
razouck <14312156+razouck@users.noreply.github.com>
2024-04-25 20:51:13 -0300

    format code and remove one comment

--- .config/nvim/lua/config/show_spaces.lua
+++ .config/nvim/lua/config/show_spaces.lua
@@ -1,35 +1,38 @@
 
-local autocmd = vim.api.nvim_create_autocmd
-local cmd     = vim.cmd
-local hl      = vim.api.nvim_set_hl
-local o       = vim.opt
-local space   = "·"
+local autocmd  = vim.api.nvim_create_autocmd
+local cmd      = vim.cmd
+local hl       = vim.api.nvim_set_hl
+local o        = vim.opt
+local space    = "·"
 
-o.list        = true
+o.list         = true
 
 o.listchars:append{
 
 	lead       = space,
 	multispace = space,
 	nbsp       = space,
-	tab        = "→" .. " ",
+	tab        = "┇" .. " ",
 	trail      = space,
 }
 
--- TODO: delete this file?
 cmd([[match TrailingWhiteSpace /\s\+$/]])
 hl(0, "TrailingWhiteSpace", { link = "Error" })
 
 autocmd("InsertEnter", {
 	callback = function()
+
 		o.listchars.trail = nil
+
 		hl(0, "TrailingWhiteSpace", { link = "Whitespace" })
 	end
 })
 
 autocmd("InsertLeave", {
 	callback = function()
+
 		o.listchars.trail = space
+
 		hl(0, "TrailingWhiteSpace", { link = "Error" })
 	end
 })
