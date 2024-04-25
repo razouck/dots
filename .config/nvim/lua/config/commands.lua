5315b5960e2595f8e26a85e60a193aaf50e10d19
razouck <14312156+razouck@users.noreply.github.com>
2024-04-25 20:51:56 -0300

    refactor the code

--- .config/nvim/lua/config/commands.lua
+++ .config/nvim/lua/config/commands.lua
@@ -1,6 +1,26 @@
 
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
-local cmd     = vim.cmd
+local api       = vim.api
+local augroup   = api.nvim_create_augroup
+local autocmd   = api.nvim_create_autocmd
+local fn        = vim.fn
+local highlight = vim.highlight
 
-cmd([[autocmd BufWritePre * %s/\s\+$//e]]) -- Remove trailing white spaces
+-- Remove trailing spaces
+autocmd('BufWritePre', { pattern = '*', command = ":%s/\\s\\+$//e" })
+
+-- Highlight on yank
+augroup('YankHighlight', { clear = true })
+autocmd('TextYankPost', {
+	group = 'YankHighlight',
+	callback = function()
+		highlight.on_yank({
+
+			higroup = (
+				fn['hlexists']('HighlightedyankRegion') > 0 and
+				'HighlightedyankRegion' or
+				'IncSearch'
+			),
+			timeout = '100'
+		})
+	end
+})
